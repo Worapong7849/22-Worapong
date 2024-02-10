fa638c6cadf08dcff50cec824f5e381445ac6fbc
Worapong Supchato <worapong78490220@gmail.com>
2024-02-10 14:20:46 +0700

    first commit

---
+++ scirpt.js
@@ -0,0 +1,8 @@
+//search input starts//
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert("ไม่มีต้องค้น");
+});
+//search input ends//
